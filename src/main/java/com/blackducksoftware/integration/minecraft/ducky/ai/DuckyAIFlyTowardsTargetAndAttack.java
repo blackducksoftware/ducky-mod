10e199b92f1ea91f099cfaaca3a9b58c4b4c6eb1
jrichard <jrichard@blackducksoftware.com>
2017-09-22 11:24:15 -0400

    Renaming variables

--- src/main/java/com/blackducksoftware/integration/minecraft/ducky/ai/DuckyAIFlyTowardsTargetAndAttack.java
+++ src/main/java/com/blackducksoftware/integration/minecraft/ducky/ai/DuckyAIFlyTowardsTargetAndAttack.java
@@ -95,12 +95,12 @@ public class DuckyAIFlyTowardsTargetAndAttack extends AbstractDuckyMoveTowardsTa
         } else {
             targetLastSeen = 0;
         }
-        final double d0 = target.posX - getDucky().posX;
-        final double d1 = target.posY + 0.2D - getDucky().posY;
-        final double d2 = target.posZ - getDucky().posZ;
-        final double xMove = (Math.signum(d0) * 0.5D - getDucky().motionX) * 0.10000000149011612D;
-        final double yMove = (d1 / 10 * 0.8D);
-        final double zMove = (Math.signum(d2) * 0.5D - getDucky().motionZ) * 0.10000000149011612D;
+        final double xDifference = target.posX - getDucky().posX;
+        final double yDifference = target.posY + 0.2D - getDucky().posY;
+        final double zDifference = target.posZ - getDucky().posZ;
+        final double xMove = (Math.signum(xDifference) * 0.5D - getDucky().motionX) * 0.10000000149011612D;
+        final double yMove = (yDifference / 10 * 0.8D);
+        final double zMove = (Math.signum(zDifference) * 0.5D - getDucky().motionZ) * 0.10000000149011612D;
 
         final float f = (float) (MathHelper.atan2(getDucky().motionZ, getDucky().motionX) * (180D / Math.PI)) - 90.0F;
         final float f1 = MathHelper.wrapDegrees(f - getDucky().rotationYaw);
